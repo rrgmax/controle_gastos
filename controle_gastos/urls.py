1e2e269b7e65dcca5c47d6e02ef84f36cb018262
Rogerio <rrgmax@bol.com.br>
2019-02-15 04:32:33 -0200

    Set nova_transacao view into controle_gastos\urls.py file

--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import home, listagem
+from contas.views import home, listagem, nova_transacao
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', listagem),
+    path('nova', nova_transacao),
     path('home', home),
 ]
