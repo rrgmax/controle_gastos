f3873817851e8c2e68836e9a6c8846c10cf11b2a
Rogerio <rrgmax@bol.com.br>
2019-02-15 04:23:41 -0200

    Define nova_trasacao function into views.py file

--- contas/views.py
+++ contas/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transacao
-from .form import Transacao
-
+from .form import TransacaoForm
 import datetime
+
 
 def home(request):
     data = {}
@@ -13,7 +13,20 @@ def home(request):
 
     return render(request, 'contas/home.html', data)
 
+
 def listagem(request):
     data = {}
     data['transacoes'] = Transacao.objects.all()
     return render(request, 'contas/listagem.html', data)
+
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
