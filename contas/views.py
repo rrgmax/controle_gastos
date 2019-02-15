b019cbefdaaa4e1faa8e706770725b049d9f732e
Rogerio <rrgmax@bol.com.br>
2019-02-15 01:42:34 -0200

    Change home function to return render into views.py

--- contas/views.py
+++ contas/views.py
@@ -1,9 +1,9 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+#from django.http import HttpResponse
 import datetime
 
 def home(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    #html = "<html><body>It is now %s.</body></html>" % now
+    return render(request, 'home')
 
