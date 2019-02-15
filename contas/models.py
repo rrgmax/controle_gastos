80bd2fb02737bf6e88dec0d2f4c2acf7307742d9
Rogerio <rrgmax@bol.com.br>
2019-02-15 02:44:20 -0200

    Create class Transacao into models.py file

--- contas/models.py
+++ contas/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Categoria(models.Model):
-    name = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
+    
+class Transacao(models.Model):
+    data = models.DateTimeField()
+    descricao = models.CharField(max_length=200)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    observacoes = models.TextField(null=True, blank=True)
