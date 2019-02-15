6e6e61fd93ca0c8eae5c28eebac49d434a7e665b
Rogerio <rrgmax@bol.com.br>
2019-02-15 02:27:31 -0200

    Set Categoria class into models.py

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    name = models.CharField(max_length=100)
+    dt_criacao = models.DateTimeField(auto_now_add=True)
