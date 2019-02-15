82182f9a7e0e78c7fbd84f893e19383ff808993e
Rogerio <rrgmax@bol.com.br>
2019-02-15 02:09:39 -0200

    Fix template does not exist error by insert the folder name into home function

--- controle_gastos/settings.py
+++ controle_gastos/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'controle_gastos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates', 'contas')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
