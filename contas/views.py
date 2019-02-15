b2bd5a323ea508c360203aa87f5113c0267112e6
Rogerio <rrgmax@bol.com.br>
2019-02-15 12:43:39 -0200

    Define delete view into views.py file

--- contas/views.py
+++ contas/views.py
@@ -44,3 +44,8 @@ def update(request, pk):
     data['form'] = form
     data['transacao'] = transacao
     return render(request, 'contas/form.html', data)
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_listagem')
