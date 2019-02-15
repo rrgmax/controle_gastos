8754ff77386446d1b694903024258d56b900724a
Rogerio <rrgmax@bol.com.br>
2019-02-15 03:18:31 -0200

    Update home function into views.py

--- contas/views.py
+++ contas/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
-    #html = "<html><body>It is now %s.</body></html>" % now
-    return render(request, 'contas/home.html')
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
 
+    data['now'] = datetime.datetime.now()
+    # html = "<html><body>It is now %s.</body></html>" % now
+
+    return render(request, 'contas/home.html', data)
